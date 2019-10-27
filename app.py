bc9f8175ece9d235e200a9dcf0240b443f9f1992
Alexander Lloyd <a.lloyd.4@bham.ac.uk>
2019-10-27 09:44:19 +0000

    feat: add unsubscribe functionality

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 
 # Emoji is hugging emoji
-YOURE_ALL_SIGNED_UP = 'Hey {name}! You are all signed up! \U0001F917'
-
+YOURE_ALL_SIGNED_UP = 'Hey {name}! You are all signed up! \U0001F917.' \
+                      ' To unsubscribe reply \'Unsubscribe\''
+UNSUBSCRIBED_MESSAGE = 'Sorry to see you go \U0001F641'
+ALREADY_SIGNED_UP = 'Hey {name}, you\' already signed up!'
 configs = {
   'development': 'config/development.py',
   'production': 'config/production.py'
@@ -41,16 +43,27 @@ def index():
 def new_user():
     from_number = request.vallues.get('From', None)
     name = request.values.get('Body', None)
+    resp = MessagingResponse()
 
-    u = User(phone_number=from_number, name=name)
-    db.session.add(u)
+    user_exists = User.query.filter_by(phone_number=from_number).first()
+    if user_exists:
+        if name.strip().lower() == 'unsubscribe':
+            # remove user from DB
+            db.session.remove(user_exists)
+            resp.message = UNSUBSCRIBED_MESSAGE
+        else:
+            name = user_exists.name
+            resp.message = ALREADY_SIGNED_UP.format(name=name)
+    else:
+        u = User(phone_number=from_number, name=name)
+        db.session.add(u)
+        resp.message(YOURE_ALL_SIGNED_UP.format(name=name))
+
     try:
         db.session.commit()
     except Exception as e:
         app.logger.exception(e)
         db.session.rollback()
-    resp = MessagingResponse()
-    resp.message(YOURE_ALL_SIGNED_UP.format(name=name))
 
     return str(resp)
 
