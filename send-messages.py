5ba35dedf01038f210074fa983edfd3404ef5e45
Alexander Lloyd <a.lloyd.4@bham.ac.uk>
2019-10-27 11:01:17 +0000

    feat: add challenges

--- send-messages.py
+++ send-messages.py
@@ -8,7 +8,13 @@ from app import User
 
 QUOTES = open('quotes.txt').read().split('\n')[0:-1]  # Last is an empty line.
 NUMBER_QUOTES = len(QUOTES)
-MESSAGE_FORMAT = 'Hey {name}! Here\'s your daily motivational quote: {quote}'
+QUOTE_FORMAT = 'Hey {name}! Here\'s your daily motivational quote: {quote}'
+
+CHALLENGE_PROBABILIIY = 5
+CHALLENGES = open('challenges.txt').read().split('\n')[0:-1]
+NUMBER_CHALLENGES = len(CHALLENGES)
+CHALL_FORMAT = 'Hey {name}! Today, we challenge yourself today: {challenge}'
+
 FROM_NUMBER = os.environ['PHONE_NUMBER']
 ACCOUNT_SID = os.environ['ACCOUNT_SID']
 AUTH_TOKEN = os.environ['ACCOUNT_TOKEN']
@@ -29,12 +35,16 @@ def send_messages():
     for user in users:
         name = user.name
         number = user.phone_number
-        quote_number = random.randint(0, NUMBER_QUOTES)
-        quote = QUOTES[quote_number]
-        send_message(FROM_NUMBER, number, MESSAGE_FORMAT.format(
-            name=name,
-            quote=quote
-        ))
+        quote_or_challenge = random.randint(0, CHALLENGE_PROBABILIIY)
+        if quote_or_challenge:
+            quote_number = random.randint(0, NUMBER_QUOTES)
+            quote = QUOTES[quote_number]
+            message = QUOTE_FORMAT.format(name=name, quote=quote)
+        else:
+            challenge_number = random.randint(0, NUMBER_CHALLENGES)
+            challenge = CHALLENGES[challenge_number]
+            message = CHALL_FORMAT.format(name=name, challenge=challenge)
+        send_message(FROM_NUMBER, number, message)
 
 
 if __name__ == '__main__':
